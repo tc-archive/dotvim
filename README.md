17317230e38fc89bbfede323c35771e6f34e234e
TempleCloud <tim.langford@gmail.com>
2017-06-21 23:44:00 +0100

    Initial check-in.

---
+++ README.md
@@ -0,0 +1,18 @@
+# dotvim
+
+Personal vim configuration
+
+### Installation 
+
+```
+git clone http://github.com/tem-os/dotvim.git ~/.vim
+
+ln -s ~/.vim/DOT-vimrc ~/.vimrc
+ln -s ~/.vim/DOT-gvimrc ~/.gvimrc
+```
+
+---
+
+### References
+
+* http://vimcasts.org/episodes/synchronizing-plugins-with-git-submodules-and-pathogen/
---
+++ scripts/vim-plug-plugins-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install vim plugins
+# vim +PlugUpdate +PlugUpgrade +PlugInstall +PlugClean +qa
+vim +PlugInstall +qall
+
+# compile YouCompleteMe plugin if declared
+YCM=$(cat ~/.vimrc | grep Plug 'Valloric/YouCompleteMe')
+if [[ -z ${YCM} ]]; then
+    pushd ~/.vim/plugged/YouCompleteMe
+    ./install.py
+    popd
+fi
